223e53e92355d6667eed3ff367536c07c6f17d75
Prado <peu_prado01@hotmail.com>
2023-08-15 00:58:39 -0300

    feat: ajuste de numeros de likes e script.js - arquivo minificado

--- js/script.js
+++ js/script.js
@@ -385,10 +385,9 @@ function redirectToAnimalProfile(animalData) {
   window.location.href = profileUrl;
 }
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRandomNumber() {
+  return Math.floor(Math.random() * 81) + 20;
 }
-
 
 async function createPostInfos(data, postElements) {
   postElements.forEach((postElement, index) => {
